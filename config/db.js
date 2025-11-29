f5498e6d91dd782d208e647b47b437d0b8983df6
Ilyeschrif22 <ilyes.chrif@esprit.tn>
2025-11-29 21:53:27 +0100

    mongo atlas config

--- config/db.js
+++ config/db.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {});
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            ssl: true,
+        });
+
         console.log('MongoDB connected');
     } catch (error) {
         console.error('MongoDB connection error:', error);
